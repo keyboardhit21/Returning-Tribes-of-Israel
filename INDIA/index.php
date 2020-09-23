4658ce94cb4fa2acc8a435f07de38c1e9ed67030
keyboardhit21 <keyboardhit21@gmail.com>
2020-09-23 12:10:19 +0800

    Added India Articles

--- INDIA/index.php
+++ INDIA/index.php
@@ -1,2 +1,62 @@
-<?php
-header("location:First%20Lesson%20to%20RTT%20Leaders%20INDIA.pdf");
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Regain Your Identity</title>
+        <link rel="stylesheet" type="text/css" href="../css/article.css"/>
+        <link rel="stylesheet" type="text/css" href="../css/style.css"/>
+        <style>
+            .container {
+                display: block;
+                width: 1100px;
+                height: auto;
+                border: 10px solid #7d7b22;
+                padding: 50px;
+            }
+            .title {
+                display: block;
+                width: 100%;
+                height: 200px;
+                /* margin-bottom: 70px; */
+            }
+            .title p {
+                margin: 0;
+                text-align: center;
+                color: #b43426;
+                font-size: 50px;
+            }
+            p {
+                font-family: coburn;
+                font-size: 35px;
+                color: #606200;
+            }
+            .list {
+                margin-bottom: 70px;
+            }
+            p a {
+                text-decoration: none;
+                color: #606200;
+            }
+            p a:hover {
+                color: #7d7b22;
+            }
+        </style>
+    </head>
+    <body>
+        <div class="container">
+            <div class="title">
+                <p>
+                    INDIA
+                </p>
+                <p style="font-size:30px">
+                    INDEX OF INDIA DOCUMENTS
+                </p>
+            </div>
+            <p class="list">
+                <a href="First%20Lesson%20to%20RTT%20Leaders%20INDIA.pdf">First Lesson to RTT Leaders INDIA</a>
+            </p>
+            <p class="list">
+                <a href="Yom%20Teruah%20Message%202020.pdf">Yom Teruah Message</a>
+            </p>
+        </div>
+    </body>
+</html>
